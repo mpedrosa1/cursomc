b2626ee1761d915090ef772a08cb117f0ff93e00
Matheus Pedrosa <matheus.pedroza@hotmail.com>
2018-05-14 13:32:53 -0300

    Estado e Cidade

---
+++ src/main/java/com/matheus/cursomc/repositories/EstadoRepository.java
@@ -0,0 +1,11 @@
+package com.matheus.cursomc.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.matheus.cursomc.domain.Estado;
+
+@Repository
+public interface EstadoRepository extends JpaRepository<Estado, Integer> {
+
+}
