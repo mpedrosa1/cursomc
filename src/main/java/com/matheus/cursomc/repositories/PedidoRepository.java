55e6d93e78ca650a398058d9c1049352dda3f65f
Matheus Pedrosa <matheus.pedroza@hotmail.com>
2018-05-17 12:32:27 -0300

    Pedido, EstadoPagamento e Pagamento

---
+++ src/main/java/com/matheus/cursomc/repositories/PedidoRepository.java
@@ -0,0 +1,11 @@
+package com.matheus.cursomc.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.matheus.cursomc.domain.Pedido;
+
+@Repository
+public interface PedidoRepository extends JpaRepository<Pedido, Integer> {
+
+}
